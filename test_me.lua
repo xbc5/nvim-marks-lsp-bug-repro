059f27ca6c5c0ba6752d4491462b9a3dbcd4d458
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-08-06 20:17:38 +0100

    fix typo

--- test_me.lua
+++ test_me.lua
@@ -1,5 +1,5 @@
 function use_hover_doc_on_me(a, b, c) end
 
 -- use :lua vim.lsp.buf.hover()
--- you will see the return type is clipped
+-- you will see the end of the window (right) is clipped
 use_hover_doc_on_me(1, 2, 3)
